3c58a57e7a21f71c3673c080b81836335b96cead
Jonathan Bartlett <jonathan.bartlett2@sky.uk>
2021-11-26 14:13:42 +0000

    Fixed bandit vulnerabilities

--- calc/app.py
+++ calc/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from markupsafe import escape
 import calc
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,7 @@ def calculations(x, y):
     return response
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", 8080, debug=True)
+    host = os.getenv("HOST", "127.0.0.1")    
+    port = int(os.getenv("PORT", 8080))
+    debug = bool(os.getenv("DEBUG", False))
+    app.run(host, port, debug=debug)
